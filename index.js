ad57c63caf4f632d5f239c19af021bf5d8d765d4
aburk3 <aburk3@gmail.com>
2018-08-30 09:55:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function takeANumber(currentLine, newName) {
+  
+  currentLine.push(newName)
+  let whereInLine = currentLine.length
+  return `Welcome, ${newName}. You are number ${whereInLine} in line.` 
+}
+
+
+
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length < 1) {
+//     return 'There is nobody waiting to be served!'
+//   }
+//   katzDeliLine.shift()
+//   return `Currently serving ${katzDeliLine[0]}.`
+// }
+
+// function nowServing(katzDeliLine) {
+//   if (katzDeliLine.length < 1) {
+//     return 'There is nobody waiting to be served!'
+//   }
+//   let removedElement = katzDeliLine.shift()
+//   return removedElement
+// }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length < 1) {
+    return 'There is nobody waiting to be served!'
+  }
+  var removedElement = katzDeliLine.shift()
+  return 'Currently serving ' + removedElement + '.'
+}
