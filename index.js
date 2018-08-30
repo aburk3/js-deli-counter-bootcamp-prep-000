2d80a217f6f52f375c1e4f2fcc6766d672937c28
aburk3 <aburk3@gmail.com>
2018-08-30 10:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ function currentLine(currentLineOfPeople) {
     return 'The line is currently empty.'
   }
   
-var text = ''
+var numberAndName = ''
 for (var i = 0; i < currentLineOfPeople.length; i++) {
-    text += currentLineOfPeople[i];
+  
   }
 }
